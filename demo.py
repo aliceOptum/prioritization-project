baae7cb6c8ef612a26f97eaab45d0deb28764f16
aliceOptum <107640297+aliceOptum@users.noreply.github.com>
2022-06-30 12:36:48 -0400

    Add files via upload

--- demo.py
+++ demo.py
@@ -8,114 +8,98 @@ import numpy as np
 
 #use pandas
 
-#code to import from csv
-dfSucc = pd.read_csv("feature-successors-5.csv")
+def run():
 
 
 
-dfPred = pd.read_csv("feature-predecessors-5.csv")
-#print(dfPred)
+    #change this value to the csv file for the successors
+    dfSucc = pd.read_csv("feature-successors-5.csv")
 
 
 
-
-dfData = pd.read_csv("export-2.csv")
-#print(dfData)
-
-#print(df1)
-
-def addToDF (dfData,index, stack):
-    current = stack.get()
-    print(current)
-    for ind in dfSucc.index:
-        if (dfSucc['ID'][ind]) == current: 
-            stack.put(dfSucc['Successor ID'][ind])
-            print("this added")
-          
-
-    found = dfData[dfData['Formatted ID'].str.contains(current)]
-    new = dfData
-    if not found.empty:
-        print("HERE")
-        cur = dfData[dfData['Formatted ID']==current]
-        print(cur.values)
-        line = pd.DataFrame(data = cur.values, columns = dfData.columns)
-        dfData = pd.concat([dfData.iloc[:index], line, dfData.iloc[index:]]).reset_index(drop=True)
-        dfData.drop_duplicates(inplace = True)
-        print("NEW VERSION")
-        print(dfData)
-   
-    return(dfData)
-  
+    #change this value to the csv file for the predecessors
+    dfPred = pd.read_csv("feature-predecessors-5.csv")
 
 
 
 
+    #change this value to the csv file from rally
+    dfData = pd.read_csv("export-2.csv")
+
+
+    def addToDF (dfData,index, stack):
+        current = stack.get()
+        for ind in dfSucc.index:
+            if (dfSucc['ID'][ind]) == current: 
+                stack.put(dfSucc['Successor ID'][ind])
+            
+
+        found = dfData[dfData['Formatted ID'].str.contains(current)]
+        new = dfData
+        if not found.empty:
+            cur = dfData[dfData['Formatted ID']==current]
+            line = pd.DataFrame(data = cur.values, columns = dfData.columns)
+            dfData = pd.concat([dfData.iloc[:index], line, dfData.iloc[index:]]).reset_index(drop=True)
+            dfData.drop_duplicates(inplace = True)
+           
+    
+        return(dfData)
     
 
 
-def modifiedBFS (dfData, current, index):
-    stack = LifoQueue(maxsize=100000000)
-    stack.put(current)
-    while stack.qsize() > 0 :
-        print("hereMOD")
-        dfData = addToDF(dfData,index, stack)
-        print("DID IT CHANGE??")
-        print(dfData)
-        print("old")
-        print(stack.qsize())
-        index += 1
-    return(dfData)
- 
 
 
-
-for ind in dfPred.index:
-    value = dfPred['ID'][ind]
-    pred =  dfPred['Predecessor ID'][ind]
-
-    if value in dfData['Formatted ID'].values and pred in dfData['Formatted ID'].values:
-    
-        index1 = dfData.loc[dfData['Formatted ID']==value].index[0]
-        print(index1)
-        predvalue = dfData[dfData['Formatted ID']==pred]
-        #print(predvalue)
-        predindex = dfData.loc[dfData['Formatted ID']==pred].index[0]
-
-
-        line = pd.DataFrame(columns = dfData.columns,data = predvalue.values)
-
-  
-
-
-
-    # pandas DaraFrame drop() Syntax
-        #dfData.drop(labels=None, axis=0, index=predindex, columns=None, level=None, inplace=True, errors='raise')
-        dfData = pd.concat([dfData.iloc[:index1], line, dfData.iloc[index1:]]).reset_index(drop=True)
-        dfData.drop_duplicates(inplace = True)
-
-
-print(len(dfData))
-
-
-
-for ind in dfData.index:
-
-    current = dfData['Formatted ID'][ind]
-    print(current)
-  
-
-    if current in dfSucc['ID'].values:
-        index1 = dfData[dfData['Formatted ID']==current].index[0]+1
-        succind = dfSucc.loc[dfSucc['ID']==current].index[0]
-        val = dfSucc['Successor ID'][succind]
-        print("this is val")
-        print(val)
-        dfData = modifiedBFS(dfData,val, index1)
-       
         
 
 
-      
-dfData.to_csv('test.csv')
-print(len(dfData))
+    def modifiedBFS (dfData, current, index):
+        stack = LifoQueue(maxsize=100000000)
+        stack.put(current)
+        while stack.qsize() > 0 :
+            dfData = addToDF(dfData,index, stack)
+            index += 1
+        return(dfData)
+    
+
+
+
+    for ind in dfPred.index:
+        value = dfPred['ID'][ind]
+        pred =  dfPred['Predecessor ID'][ind]
+
+        if value in dfData['Formatted ID'].values and pred in dfData['Formatted ID'].values:
+        
+            index1 = dfData.loc[dfData['Formatted ID']==value].index[0]
+            predvalue = dfData[dfData['Formatted ID']==pred]
+            predindex = dfData.loc[dfData['Formatted ID']==pred].index[0]
+
+
+            line = pd.DataFrame(columns = dfData.columns,data = predvalue.values)
+
+    
+     
+            dfData = pd.concat([dfData.iloc[:index1], line, dfData.iloc[index1:]]).reset_index(drop=True)
+            dfData.drop_duplicates(inplace = True)
+
+
+
+
+
+    for index, row in dfData.iterrows():
+
+        current = row['Formatted ID']
+
+    
+
+        if current in dfSucc['ID'].values:
+            index1 = dfData[dfData['Formatted ID']==current].index[0]+1
+            succind = dfSucc.loc[dfSucc['ID']==current].index[0]
+            val = dfSucc['Successor ID'][succind]
+            dfData = modifiedBFS(dfData,val, index1)
+        
+            
+
+
+        
+    dfData.to_csv('test.csv')
+    
